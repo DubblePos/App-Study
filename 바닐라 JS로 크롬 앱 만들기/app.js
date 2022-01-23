8ce157ae4003d3265917c10380be156a10203c3a
Double.Pos <wks1945@naver.com>
2022-01-23 19:06:12 +0900

    login innerText return

--- 바닐라 JS로 크롬 앱 만들기/app.js
+++ 바닐라 JS로 크롬 앱 만들기/app.js
@@ -1,12 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden"; //String 만 있을때 주소 대분자로 작성
 
 function onLoginSubmit(event){
     event.preventDefault();
-    //const username = loginInput.value;
-    console.log(loginInput.value);
-    
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    //greeting.innerText = "Hello " + username; 1번쨰방법
+    greeting.innerText = `Hello ${username}`; //2번째 방법 맥은 영어로 바꾸고하면됨
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 
 loginForm.addEventListener("submit",onLoginSubmit);
