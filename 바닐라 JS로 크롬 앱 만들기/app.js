8dd9a68d8e73f62f06ce7bf3a341c52049b1de9c
Double.Pos <wks1945@naver.com>
2022-01-23 20:51:04 +0900

    Loading Username

--- 바닐라 JS로 크롬 앱 만들기/app.js
+++ 바닐라 JS로 크롬 앱 만들기/app.js
@@ -3,16 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; //String 만 있을때 주소 대분자로 작성
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     //greeting.innerText = "Hello " + username; 1번쨰방법
-    greeting.innerText = `Hello ${username}`; //2번째 방법 맥은 영어로 바꾸고하면됨
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit",onLoginSubmit);
+console.log(savedUsername);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
