5b41fed890a13c46bb6f17b5dd9d75291362339f
Double.Pos <wks1945@naver.com>
2022-01-23 23:00:58 +0900

    PadStart

--- 바닐라 JS로 크롬 앱 만들기/js/clock.js
+++ 바닐라 JS로 크롬 앱 만들기/js/clock.js
@@ -2,11 +2,15 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0"); //padStart : 글자수가 2가 될떄까지 0을 추가한다.
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-getClock();
-setInterval(getClock, 1000);
+getClock(); //즉시호출
+setInterval(getClock, 1000); // 1초마다 갱신
 
 //setInterval(sayHello, 5000); setInterval : 매초 반복적으로 무언가를 보여줄려고할떄?
 
