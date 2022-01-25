5ed57f200cccc77d34098deb6f737b29c6bf649f
Double.Pos <wks1945@naver.com>
2022-01-25 16:45:48 +0900

    Deleting ToDos

--- 바닐라 JS로 크롬 앱 만들기/js/todo.js
+++ 바닐라 JS로 크롬 앱 만들기/js/todo.js
@@ -2,14 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    // console.dir(event.target.parentElement.innerText); 정보보기가 쉽다. 클릭된 element부모 찾기
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
-
 }
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
