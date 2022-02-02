51e36685c935b41d4a05675925f0aaff0b1ff803
Double.Pos <wks1945@naver.com>
2022-02-02 17:49:43 +0900

    ToDo List part One

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,24 +1,29 @@
-import { useState, useEffect} from "react";
+import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time!");
-  useEffect(()=>{
-    console.log("CALL THE API");
-  }, []);
-  useEffect(()=>{
-    if(keyword !== "" && keyword.length > 5){
-    console.log("SEARCH FOR", keyword);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword]);
+    setToDos((currentArray) => [toDo, ...currentArray]);//toDo를 currentArray에 추가하려면 ...
+    setToDo("");
+  };
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
