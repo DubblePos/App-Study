775000667447f97da8b659eda6d89ac39f075fd0
Double.Pos <wks1945@naver.com>
2022-02-03 03:18:18 +0900

    Parameters

--- practice/src/routes/Detail.js
+++ practice/src/routes/Detail.js
@@ -1,4 +1,23 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const getMovie = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  }, [id]);
+  console.log(movie);
+  useEffect(() => {
+    getMovie();
+  }, [getMovie]);
+  return <div>으잉 어렵다.</div>;
 }
+
 export default Detail;
