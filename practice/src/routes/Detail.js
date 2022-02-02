6885f555d1b025b576f3a13a3e2034f1236cf85e
Double.Pos <wks1945@naver.com>
2022-02-03 03:34:16 +0900

    Parameters Detail 완료

--- practice/src/routes/Detail.js
+++ practice/src/routes/Detail.js
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Movie from "../components/Movie";
+import PropTypes from "prop-types";
 
 function Detail() {
   const [loading, setLoading] = useState(true);
@@ -17,7 +17,40 @@ function Detail() {
   useEffect(() => {
     getMovie();
   }, [getMovie]);
-  return <div>으잉 어렵다.</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>{movie.title}</h1>
+          <img src={movie.large_cover_image} alt={movie.title} />
+          <h2>평점 : {movie.rating}</h2>
+          <h2>러닝타임 : {movie.runtime}분</h2>
+          <h3>개봉일 : {movie.year}</h3>
+          <p>
+            줄거리 <hr />
+            {movie.description_full}
+          </p>
+          <ul>
+            <h1>장르</h1>
+            <br />
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
+
+Detail.propTypes = {
+  id: PropTypes.number.isRequired,
+  large_cover_image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Detail;
